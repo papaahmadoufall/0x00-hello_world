625fd9c3b347a457f1bbe83d5b2ff749428abe0c
papa ahmadou fall <pahmadoudou@gmail.com>
2022-07-01 18:08:12 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n, m, y, y;
+
+	n = 65;
+	m = 97;
+	x = 97;
+	y = 122;
+	/* LOWERCASE ALPHABET LOOP */
+	while (x <= y)
+	{
+		putchar(x);
+		x++;
+	}
+	/* LOWERCASE ALPHABET LOOP */
+	while (n <= m)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar(10);
+	return (0);
+}
