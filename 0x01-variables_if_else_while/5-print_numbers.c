ab1285d950a3832fbf55d6eac982d52a33aaa64e
papa ahmadou fall <pahmadoudou@gmail.com>
2022-07-01 18:28:42 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i = 0;
+	/* your code goes there */
+	while (i < 10)
+	{
+		printf(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
+
